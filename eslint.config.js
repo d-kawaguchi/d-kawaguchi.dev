15dedde15875828f2a798d6b923c7c9d5d5b5807
d-kawaguchi <45481012+d-kawaguchi@users.noreply.github.com>
2024-05-08 20:36:25 +0900

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,30 +2,31 @@ import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import tsEsLintPlugin from "@typescript-eslint/eslint-plugin";
 import tsEsLintParser from "@typescript-eslint/parser";
-import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 
 export default [
-    js.configs.recommended, // eslint:recommended
-    eslintConfigPrettier,
-    {
-        plugins: {
-            "@typescript-eslint": tsEsLintPlugin,
-            unicorn: eslintPluginUnicorn,
-        },
+  js.configs.recommended, // eslint:recommended
+  eslintConfigPrettier,
+  {
+    plugins: {
+      "@typescript-eslint": tsEsLintPlugin,
+      unicorn: eslintPluginUnicorn,
     },
-    {
-        files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
-        languageOptions: {
-            parser: tsEsLintParser,
-            parserOptions: {
-                project: true,
-            },
-        },
-        rules: {
-            ...tsEsLintPlugin.configs["eslint-recommended"].overrides[0].rules,
-            ...tsEsLintPlugin.configs["recommended-type-checked"].rules,
-            "@typescript-eslint/no-explicit-any": "error",
-            ...eslintPluginUnicorn.configs["recommended"].rules
-        }
-    }
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.cts", "**/*.mts"],
+    languageOptions: {
+      parser: tsEsLintParser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    rules: {
+      ...tsEsLintPlugin.configs["eslint-recommended"].overrides[0].rules,
+      ...tsEsLintPlugin.configs["recommended-type-checked"].rules,
+      "@typescript-eslint/no-explicit-any": "error",
+      ...eslintPluginUnicorn.configs["recommended"].rules,
+      "unicorn/prevent-abbreviations": "off",
+    },
+  },
 ];
